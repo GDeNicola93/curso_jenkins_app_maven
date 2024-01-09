7eac8941084468d8cd1536ed74226070582e4735
Germán De Nicola <denicolagerman@gmail.com>
2024-01-09 12:13:54 -0300

    Se cambia el proyecto acorde al del ejemplo del curso

--- src/main/java/com/curso_jenkins/prueba_app_maven/PruebaAppMavenApplication.java
+++ src/main/java/com/curso_jenkins/prueba_app_maven/PruebaAppMavenApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PruebaAppMavenApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PruebaAppMavenApplication.class, args);
+		System.out.println("Hello World");
 	}
 
 }
